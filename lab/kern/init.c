6a70c73d721cd169dad530198e6c90857945b2b6
ASUS <309410281@qq.com>
2020-09-23 20:43:32 +0800

    add lab2 Lab 2: Memory Management

--- lab/kern/init.c
+++ lab/kern/init.c
@@ -6,8 +6,9 @@
 
 #include <kern/monitor.h>
 #include <kern/console.h>
+#include <kern/pmap.h>
+#include <kern/kclock.h>
 
-// Test the stack backtrace function (lab 1 only)
 void
 test_backtrace(int x)
 {
@@ -36,7 +37,10 @@ i386_init(void)
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
 	// Test the stack backtrace function (lab 1 only)
-	test_backtrace(5);
+	//test_backtrace(5);
+	// Lab 2 memory management initialization functions
+	mem_init();
+	
 
 	// Drop into the kernel monitor.
 	while (1)
